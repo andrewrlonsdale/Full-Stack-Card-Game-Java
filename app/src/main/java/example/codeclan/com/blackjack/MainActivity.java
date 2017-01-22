fc1f9d19ef3fbf058732a7491e26df238e814ea1
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-22 13:03:41 +0000

    add menu

--- app/src/main/java/example/codeclan/com/blackjack/MainActivity.java
+++ app/src/main/java/example/codeclan/com/blackjack/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -48,5 +51,20 @@ public class MainActivity extends AppCompatActivity {
     public void onOptionsButtonPressed(View button){
         Log.d(getClass().toString(),"Options button pressed");
         startActivity(intent3);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_option) {
+            startActivity(intent3);
+            Log.d(getClass().toString(), "Options selected");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
