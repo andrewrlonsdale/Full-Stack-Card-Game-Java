5f19149456b3ae6ef198a95a1b4d75f1d8408514
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-22 15:06:05 +0000

    add Suit class

---
+++ app/src/main/java/example/codeclan/com/blackjack/Suit.java
@@ -0,0 +1,22 @@
+package example.codeclan.com.blackjack;
+
+/**
+ * Created by user on 22/01/2017.
+ */
+
+public enum Suit {
+
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+};
