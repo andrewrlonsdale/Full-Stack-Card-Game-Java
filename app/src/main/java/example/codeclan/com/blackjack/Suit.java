ba1350de716941b5c3b16180576164dc8941ad5a
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-22 15:14:49 +0000

    add values to Suit class

--- app/src/main/java/example/codeclan/com/blackjack/Suit.java
+++ app/src/main/java/example/codeclan/com/blackjack/Suit.java
@@ -6,17 +6,8 @@ package example.codeclan.com.blackjack;
 
 public enum Suit {
 
-        ACE,
-        TWO,
-        THREE,
-        FOUR,
-        FIVE,
-        SIX,
-        SEVEN,
-        EIGHT,
-        NINE,
-        TEN,
-        JACK,
-        QUEEN,
-        KING
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
 };
