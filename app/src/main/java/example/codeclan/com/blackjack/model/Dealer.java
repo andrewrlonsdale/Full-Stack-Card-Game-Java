c2010cc917faabb6a66106643c0b009d03926b4b
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-23 13:22:49 +0000

    add logic to Dealer

--- app/src/main/java/example/codeclan/com/blackjack/model/Dealer.java
+++ app/src/main/java/example/codeclan/com/blackjack/model/Dealer.java
@@ -4,7 +4,11 @@ package example.codeclan.com.blackjack.model;
  * Created by user on 23/01/2017.
  */
 
-public class Dealer {
+public class Dealer extends User {
+
+    public Dealer(Hand hand) {
+        super(hand);
+    }
 
 
 }
