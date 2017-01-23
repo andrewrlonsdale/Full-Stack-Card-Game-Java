718d32dc066f2158a8949cec1c7e74e5f088b53e
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-23 18:47:43 +0000

    add drawable files

--- app/src/main/java/example/codeclan/com/blackjack/Activity/GameActivity.java
+++ app/src/main/java/example/codeclan/com/blackjack/Activity/GameActivity.java
@@ -4,19 +4,32 @@ package example.codeclan.com.blackjack.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import example.codeclan.com.blackjack.R;
+import example.codeclan.com.blackjack.model.Game;
 
 /**
  * Created by user on 21/01/2017.
  */
 public class GameActivity extends AppCompatActivity {
 
+    private Game game;
     Intent intent3;
+
+
+
+    public GameActivity() {
+        game = new Game();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
