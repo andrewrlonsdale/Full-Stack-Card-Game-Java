bf64c9664f3545168202e06277a16d3f0e2262f2
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-23 14:45:58 +0000

    add logic to Game

--- app/src/main/java/example/codeclan/com/blackjack/model/Game.java
+++ app/src/main/java/example/codeclan/com/blackjack/model/Game.java
@@ -5,4 +5,85 @@ package example.codeclan.com.blackjack.model;
  */
 
 public class Game {
+
+    private Dealer dealer;
+    private Player player;
+    private Deck deck;
+
+    public enum Outcome {
+        PLAYER, DEALER, DRAW
+    }
+
+    public Game() {
+        deck = new Deck();
+        dealer = new Dealer(deck.createStartHand());
+        player = new Player(deck.createStartHand());
+    }
+
+    public void dealAgain() {
+        deck = new Deck();
+        dealer.setHand(deck.createStartHand());
+        player.setHand(deck.createStartHand());
+    }
+
+    public Hand getDealerHand() {
+        return dealer.getHand();
+    }
+
+    public Hand getPlayerHand() {
+        return player.getHand();
+    }
+
+    public int getDealerScore() {
+        return dealer.getHand().getScore();
+    }
+
+    public int getPlayerScore() {
+        return player.getHand().getScore();
+    }
+
+    public boolean playerHasBlackjack() {
+        return player.hasBlackjack();
+    }
+
+    public boolean dealerHasBusted() {
+        return dealer.hasBusted();
+    }
+
+    public boolean playerHasBusted() {
+        return player.hasBusted();
+    }
+
+    public boolean dealerShouldDrawCard() {
+        return dealer.shouldDrawCard();
+    }
+
+    public void dealerShowHoleCard() {
+        dealer.showHoleCard();
+    }
+
+    public void dealDealerCard() {
+        dealer.dealCard(deck.getRandomCard());
+    }
+
+    public void dealPlayerCard() {
+        player.dealCard(deck.getRandomCard());
+    }
+
+    public void resetPlayersHands() {
+        dealer.resetHand();
+        player.resetHand();
+    }
+
+
+    public Outcome getOutcome() {
+        int playerScore = player.getHand().getScore();
+        int dealerScore = dealer.getHand().getScore();
+
+        if (playerScore == dealerScore) {
+            return Outcome.DRAW;
+        }
+
+        return (playerScore > dealerScore) ? Outcome.PLAYER : Outcome.DEALER;
+    }
 }
