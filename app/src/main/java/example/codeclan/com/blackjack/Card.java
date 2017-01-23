431588ec3e96ab302c16ffb935763d3ac56601b1
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-23 00:54:34 +0000

    add Card class

---
+++ app/src/main/java/example/codeclan/com/blackjack/Card.java
@@ -0,0 +1,34 @@
+package example.codeclan.com.blackjack;
+
+/**
+ * Created by user on 23/01/2017.
+ */
+
+public class Card {
+
+    private Suit suit;
+    private Value value;
+    private boolean isVisible;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+        this.isVisible = false;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void showCard() {
+        this.isVisible = true;
+    }
+}
