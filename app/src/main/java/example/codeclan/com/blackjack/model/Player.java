ef7be2344eda3ec0ab41d023198c4a14f5a304b7
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-23 13:21:45 +0000

    add logic to player

--- app/src/main/java/example/codeclan/com/blackjack/model/Player.java
+++ app/src/main/java/example/codeclan/com/blackjack/model/Player.java
@@ -4,5 +4,23 @@ package example.codeclan.com.blackjack.model;
  * Created by user on 23/01/2017.
  */
 
-public class Player {
+public class Player extends User{
+
+    public Player(Hand hand) {
+        super(hand);
+        showAllCards();
+    }
+
+    @Override
+    public void setHand(Hand hand) {
+        super.setHand(hand);
+        showAllCards();
+    }
+
+    private void showAllCards() {
+        for (Card card : getHand().getCards()) {
+            card.showCard();
+        }
+    }
+}
 }
