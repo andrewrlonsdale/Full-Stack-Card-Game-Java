b20a6a0acc84842b3596c9f2f71b422a271997b6
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-23 11:45:26 +0000

    add background colours

--- app/src/main/java/example/codeclan/com/blackjack/OptionsActivity.java
+++ app/src/main/java/example/codeclan/com/blackjack/OptionsActivity.java
@@ -1,13 +1,22 @@
 package example.codeclan.com.blackjack;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import static example.codeclan.com.blackjack.R.color.color_blue;
+import static example.codeclan.com.blackjack.R.color.color_green;
+import static example.codeclan.com.blackjack.R.color.color_red;
 
 /**
  * Created by user on 21/01/2017.
@@ -15,11 +24,19 @@ import android.view.MenuItem;
 public class OptionsActivity extends AppCompatActivity {
 
     Intent intent3;
+    RadioButton green;
+    RadioButton red;
+    RadioButton blue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        green = (RadioButton)findViewById(R.id.radioButton1);
+        red = (RadioButton)findViewById(R.id.radioButton2);
+        blue = (RadioButton)findViewById(R.id.radioButton3);
 
         intent3 = new Intent(OptionsActivity.this, OptionsActivity.class);
 
@@ -41,5 +58,20 @@ public class OptionsActivity extends AppCompatActivity {
             Log.d(getClass().toString(), "Options selected");
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onGreenButtonClick(View view) {
+        Log.d("PersistenceExample:", "green Button Clicked!");
+            getResources().getColor(color_green);
+    }
+
+    public void onRedButtonClick(View view) {
+        Log.d("PersistenceExample:", "red Button Clicked!");
+        getResources().getColor(color_red);
+    }
+
+    public void onBlueButtonClick(View view) {
+        Log.d("PersistenceExample:", "blue Button Clicked!");
+        getResources().getColor(color_blue);
     }
 }
