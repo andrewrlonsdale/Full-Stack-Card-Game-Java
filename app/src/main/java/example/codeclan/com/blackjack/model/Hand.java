7a052b4b5867f02fb8c8b41d036e20baab1d3462
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-23 12:23:30 +0000

    add logic to Hand

--- app/src/main/java/example/codeclan/com/blackjack/model/Hand.java
+++ app/src/main/java/example/codeclan/com/blackjack/model/Hand.java
@@ -1,8 +1,22 @@
 package example.codeclan.com.blackjack.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by user on 23/01/2017.
  */
 
 public class Hand {
+
+    public static final int LIMIT = 21;
+
+    private List<Card> cards;
+    private int sum = 0;
+
+    public Hand(List<Card> cards) {
+        this.cards = cards;
+
+    }
+
 }
--- app/src/main/java/example/codeclan/com/blackjack/model/User.java
+++ app/src/main/java/example/codeclan/com/blackjack/model/User.java
@@ -4,5 +4,7 @@ package example.codeclan.com.blackjack.model;
  * Created by user on 23/01/2017.
  */
 
-public class User {
+public abstract class User {
+
+
 }
