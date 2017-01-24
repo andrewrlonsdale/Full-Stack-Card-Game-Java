2e74cb2010a81208cbc6e3b7ca37ba9db0c241f1
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-24 10:36:16 +0000

    add vibration_service

--- app/src/main/java/example/codeclan/com/blackjack/OptionsActivity.java
+++ app/src/main/java/example/codeclan/com/blackjack/OptionsActivity.java
@@ -1,17 +1,20 @@
 package example.codeclan.com.blackjack;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 
-import example.codeclan.com.blackjack.R;
 
 import static example.codeclan.com.blackjack.R.color.color_blue;
 import static example.codeclan.com.blackjack.R.color.color_green;
@@ -20,10 +23,13 @@ import static example.codeclan.com.blackjack.R.color.color_red;
 /**
  * Created by user on 21/01/2017.
  */
-public class OptionsActivity extends AppCompatActivity {
+public class OptionsActivity extends AppCompatActivity implements View.OnClickListener {
 
     Intent intent3;
     RadioButton green, red, blue, deck1, deck2, deck3;
+    private Button vibrateButton;
+
+    private Vibrator vibrator;
 
 
     @Override
@@ -31,6 +37,9 @@ public class OptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
+        vibrator =(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        vibrateButton = (Button)findViewById(R.id.vibrate);
         green = (RadioButton)findViewById(R.id.radioButton1);
         red = (RadioButton)findViewById(R.id.radioButton2);
         blue = (RadioButton)findViewById(R.id.radioButton3);
@@ -38,11 +47,15 @@ public class OptionsActivity extends AppCompatActivity {
         deck2 = (RadioButton)findViewById(R.id.radioButton5);
         deck3 = (RadioButton)findViewById(R.id.radioButton6);
 
+        vibrateButton.setOnClickListener(this);
+
         intent3 = new Intent(OptionsActivity.this, OptionsActivity.class);
 
         Log.d(getClass().toString(), "on create called");
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,10 +73,12 @@ public class OptionsActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onGreenButtonClick(View view) {
-        Log.d("PersistenceExample:", "green Button Clicked!");
-            getResources().getColor(color_green);
-    }
+//    public void onGreenButtonClick(RadioButton button) {
+//        Log.d("PersistenceExample:", "green Button Clicked!");
+//        if (button.getItemId() == R.id.radioButton1) {
+//            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+//        }
+//    }
 
     public void onRedButtonClick(View view) {
         Log.d("PersistenceExample:", "red Button Clicked!");
@@ -90,5 +105,13 @@ public class OptionsActivity extends AppCompatActivity {
         getResources().getColor(color_blue);
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case    R.id.vibrate:
+                vibrator.vibrate(1000);
+        }
+    }
 
 }
